d46ed62c8da78c24a25eac1013358b1e515ef958
Juan Pablo Moreno <jpmoreno98@gmail.com>
2023-09-21 09:26:18 -0600

    zFunction

---
+++ zFunction/zFunction.cpp
@@ -0,0 +1,53 @@
+// Z function
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector <int> zFunction(std::string pattern){
+    int patternLength = pattern.length();
+    std::vector <int> zVector(patternLength);
+    int x = 0;
+    int y = 0;
+    zVector[0] = 0;
+    for (int i = 1; i < patternLength; i++){
+        if(i > y){
+            x = y = i;
+            while(y < patternLength && pattern[y] == pattern[y-x]){
+                y++;
+            }
+            zVector[i] = y - x;
+            y--;
+        }
+        else{
+            int k = i - x;
+            if(zVector[k] < y - i + 1){
+                zVector[i] = zVector[k];
+            }
+            else{
+                x = i;
+                while(y < patternLength && pattern[y] == pattern[y-x]){
+                    y++;
+                }
+                zVector[i] = y - x;
+                y--;
+            }
+        }
+    }
+    return zVector;
+}
+
+
+
+int main() {
+    std::string pattern_test = "ABCABC$XABCABXABCABCX";
+    std::vector<int> vector_test = zFunction(pattern_test);
+
+    for (int i = 0; i < pattern_test.length(); i++) {
+        std::cout << "Position " << i << ": ";
+        std::cout << "Pattern Value = " << pattern_test[i] << ", ";
+        std::cout << "Z-Value = " << vector_test[i] << std::endl;
+    }
+
+    return 0;
+}
