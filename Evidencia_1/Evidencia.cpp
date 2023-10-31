8fa4cf83acb04de99c6d52b0bac3f6ac92566840
Domingo Mora <A01783317.tec.mx>
2023-10-31 16:41:57 -0600

    Nombre

--- Evidencia_1/Evidencia.cpp
+++ Evidencia_1/Evidencia.cpp
@@ -2,7 +2,7 @@
 /*Sylvia Fernanda Colomo Fuente -A01781983*/
 /*Shaul Zayat Askenazi - A01783240*/
 /*Juan Pablo Moreno Robles Arenas - A01374091*/
-/*Domingo Mora*/
+/*Domingo Mora - A01783317*/
 
 
 #include <iostream>
